a8c2b91e8c320077c0e2bea3e8f60cfddfd41ab7
Jason Mashinchi <jasonmash@users.noreply.github.com>
2018-12-06 13:50:23 +0000

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Logo" src="../assets/logo.png">
+    <HomeContent msg="Welcome to Voyager"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import HomeContent from "@/components/Home.vue"; // @ is an alias to /src
+
+@Component({
+  components: {
+    HomeContent,
+  },
+})
+export default class Home extends Vue {}
+</script>
