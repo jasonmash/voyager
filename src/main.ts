82dec49193bc8a28ea27b2f1f41ca4b59a295159
Jason Mashinchi <jasonmash@users.noreply.github.com>
2019-01-25 00:05:32 +0000

    Improve linting

--- src/main.ts
+++ src/main.ts
@@ -12,5 +12,5 @@ Vue.use(BootstrapVue);
 
 new Vue({
   router,
-  render: (h) => h(App),
+  render: (h) => h(App)
 }).$mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home text-center">
     <img alt="Logo" src="../assets/logo.png">
-    <HomeContent msg="Welcome to Voyager"/>
+    <HomeContent msg="Project (codename voyager)"/>
   </div>
 </template>
 
@@ -11,8 +11,8 @@ import HomeContent from "@/components/Home.vue"; // @ is an alias to /src
 
 @Component({
   components: {
-    HomeContent,
-  },
+    HomeContent
+  }
 })
 export default class Home extends Vue {}
 </script>
